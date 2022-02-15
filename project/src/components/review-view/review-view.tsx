99d678665bbbd80998deb323649ee206dfb83217
Aleksei Nosikov <gronula@yandex.ru>
2022-02-15 15:25:50 +0300

    Updates review view component

--- project/src/components/review-view/review-view.tsx
+++ project/src/components/review-view/review-view.tsx
@@ -1,4 +1,6 @@
 function ReviewView(): JSX.Element {
+  const RATING_ITEMS = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
+
   return (
     <section className="film-card film-card--full">
       <div className="film-card__header">
@@ -85,146 +87,26 @@ function ReviewView(): JSX.Element {
         >
           <div className="rating">
             <div className="rating__stars">
-              <input
-                className="rating__input"
-                id="star-10"
-                type="radio"
-                name="rating"
-                value="10"
-              />
-              <label
-                className="rating__label"
-                htmlFor="star-10"
-              >
-                Rating 10
-              </label>
-
-              <input
-                className="rating__input"
-                id="star-9"
-                type="radio"
-                name="rating"
-                value="9"
-              />
-              <label
-                className="rating__label"
-                htmlFor="star-9"
-              >
-                Rating 9
-              </label>
-
-              <input
-                className="rating__input"
-                id="star-8"
-                type="radio"
-                name="rating"
-                value="8"
-                checked
-              />
-              <label
-                className="rating__label"
-                htmlFor="star-8"
-              >
-                Rating 8
-              </label>
-
-              <input
-                className="rating__input"
-                id="star-7"
-                type="radio"
-                name="rating"
-                value="7"
-              />
-              <label
-                className="rating__label"
-                htmlFor="star-7"
-              >
-                Rating 7
-              </label>
-
-              <input
-                className="rating__input"
-                id="star-6"
-                type="radio"
-                name="rating"
-                value="6"
-              />
-              <label
-                className="rating__label"
-                htmlFor="star-6"
-              >
-                Rating 6
-              </label>
-
-              <input
-                className="rating__input"
-                id="star-5"
-                type="radio"
-                name="rating"
-                value="5"
-              />
-              <label
-                className="rating__label"
-                htmlFor="star-5"
-              >
-                Rating 5
-              </label>
-
-              <input
-                className="rating__input"
-                id="star-4"
-                type="radio"
-                name="rating"
-                value="4"
-              />
-              <label
-                className="rating__label"
-                htmlFor="star-4"
-              >
-                Rating 4
-              </label>
-
-              <input
-                className="rating__input"
-                id="star-3"
-                type="radio"
-                name="rating"
-                value="3"
-              />
-              <label
-                className="rating__label"
-                htmlFor="star-3"
-              >
-                Rating 3
-              </label>
-
-              <input
-                className="rating__input"
-                id="star-2"
-                type="radio"
-                name="rating"
-                value="2"
-              />
-              <label
-                className="rating__label"
-                htmlFor="star-2"
-              >
-                Rating 2
-              </label>
-
-              <input
-                className="rating__input"
-                id="star-1"
-                type="radio"
-                name="rating"
-                value="1"
-              />
-              <label
-                className="rating__label"
-                htmlFor="star-1"
-              >
-                Rating 1
-              </label>
+              {
+                RATING_ITEMS.map((item) => (
+                  <>
+                    <input
+                      className="rating__input"
+                      id={`star-${item}`}
+                      type="radio"
+                      name="rating"
+                      value={item}
+                      checked={item === 8}
+                    />
+                    <label
+                      className="rating__label"
+                      htmlFor={`star-${item}`}
+                    >
+                      Rating {item}
+                    </label>
+                  </>
+                ))
+              }
             </div>
           </div>
 
