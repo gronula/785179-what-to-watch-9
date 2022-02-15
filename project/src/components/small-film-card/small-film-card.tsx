b1a90a92bc8a121a3562ad89de1a9f6c5db09cc1
Aleksei Nosikov <gronula@yandex.ru>
2022-02-15 15:13:40 +0300

    Updates small film card component

--- project/src/components/small-film-card/small-film-card.tsx
+++ project/src/components/small-film-card/small-film-card.tsx
@@ -1,32 +1,40 @@
-type Poster = {
-  alt: string;
-  height?: number;
-  src: string;
-  width?: number;
-}
-type SmallFilmCardProps = {
+type BaseProps = {
   className?: string;
-  href: string;
-  poster: Poster;
-  title: string;
 }
+
+type SmallFilmCardProps = BaseProps & {
+  href?: string;
+  name: string;
+  posterImage: string;
+  posterSize?: 'medium';
+}
+
+const PosterSize = {
+  medium: {
+    height: 175,
+    width: 280,
+  },
+};
 
 function SmallFilmCard(
   {
     className = '',
-    href,
-    poster,
-    title,
+    href = 'film-page.html',
+    name,
+    posterImage,
+    posterSize = 'medium',
   }: SmallFilmCardProps,
 ): JSX.Element {
+  const { width, height } = PosterSize[posterSize];
+
   return (
     <article className={`small-film-card ${className}`} >
       <div className="small-film-card__image">
         <img
-          src={poster.src}
-          alt={poster.alt}
-          width={poster.width || 280}
-          height={poster.height || 175}
+          src={posterImage}
+          alt={name}
+          width={width}
+          height={height}
         />
       </div>
 
@@ -35,7 +43,7 @@ function SmallFilmCard(
           className="small-film-card__link"
           href={href}
         >
-          {title}
+          {name}
         </a>
       </h3>
     </article>
